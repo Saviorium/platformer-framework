0ac36e812e3dfce951d9dd1f77100a3d2d609541
sawa9889 <36417394+sawa9889@users.noreply.github.com>
2020-10-05 00:30:05 +0300

    Add physics (#1)

--- engine/utils.lua
+++ engine/utils.lua
@@ -3,6 +3,7 @@ function math.clamp(min, value, max)
     return math.max(min, math.min(max, value))
 end
 
+<<<<<<< HEAD
 function findIn(table, object)
 	local index = -1
     for ind, name2 in pairs(table) do
@@ -11,4 +12,18 @@ function findIn(table, object)
     	end
     end
     return index
+=======
+function getIndex(table, object)
+    local index = -1
+        for ind, name2 in pairs(table) do
+            if object == name2 then
+                index = ind
+            end
+        end
+    return index
+end
+
+function isIn(table, object)
+    return getIndex(table, object) ~= -1
+>>>>>>> Add physics (#1)
 end
