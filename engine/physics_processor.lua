4e3bc07b5df84286de06f283e47ce0d182e4c36d
sawa9889 <36417394+sawa9889@users.noreply.github.com>
2020-10-27 00:51:39 +0300

    Add physics (#1)

--- engine/physics_processor.lua
+++ engine/physics_processor.lua
@@ -4,12 +4,12 @@ PhysicsObject = require "game.physics.physics_object"
 Images = require "resource.images"
 
 PhysicsProcessor = Class {
-    init = function(self, CollisionProcessor, MovingProcessor, Animator)
-        self.CollisionProcessor = CollisionProcessor
-        self.MovingProcessor = MovingProcessor
-        self.Animator = Animator
+    init = function(self, movingProcessor)
+        self.movingProcessor = MovingProcessor
+        self.globalGravity = Vector( 0, 1)
         self.objects = {}
         self.layers = {}
+        self.objectsTypes = {}
     end
 }
 
@@ -17,109 +17,114 @@ function PhysicsProcessor:addLayer( newLayer )
     for ind, layer in pairs(self.layers) do
         table.insert( layer.collidedLayers, newLayer.name )
     end
+}
+
+function PhysicsProcessor:addType( newType )
+    table.insert( self.objectsTypes, newType )
+end
+
+function PhysicsProcessor:registerObjectType( typeName, gravity, maxSpeed, isColliding  )
+    local newType = {   
+                        name = typeName,
+                        gravity = gravity and gravity or self.globalGravity
+                        maxSpeed = maxSpeed and maxSpeed or 10,
+                        isColliding = isColliding and isColliding or true,
+                     }
+    self:addLayer(newType)
+end
+
+
+function PhysicsProcessor:allLayerNames()
+    local newCollidedLayers = {}
+    for ind, layer in pairs(self.layers) do
+        table.insert( newCollidedLayers, layer.name )
+    end
+    return newCollidedLayers
+end
+
+function PhysicsProcessor:addLayer( newLayer )
     table.insert( self.layers, newLayer )
 end
 
 function PhysicsProcessor:registerLayer( layerName, gravity )
-	-- Добавляет в обработчика всех колизий новый слой, с указанной гравитацией и двумя пустыми списками - CollidedLayers, ActionLayers
-	local newLayer = {	
-					    name = layerName,
-					    gravityEnabled = gravity
-					    collidedLayers = {},
-					    actionLayers = {},
-					 }
-    for ind, layer in pairs(self.layers) do
-        table.insert( newLayer.collidedLayers, layer.name )
-    end			 
-	self:addLayer(newLayer)
+    -- Добавляет в обработчика всех колизий новый слой, с указанной гравитацией и двумя пустыми списками - CollidedLayers, ActionLayers
+    local newLayer = {  
+                        name = layerName,
+                        gravityEnabled = gravity and gravity or true
+                        collidedLayers = {},
+                        actionLayers = {},
+                     }
+    self:addLayer(newLayer)
 end
 
 function PhysicsProcessor:addCollidedLayers( layerName, collidedLayersNames )
-	-- Добавить для слоя слои от которых он отталкивается
     for _, name in pairs(collidedLayersNames) do
-    	local index = findIn(self.layers, name)
-        if index == -1 then
-        	-- Удалить слой из списка
+        if isIn(self.layers, name) and not isIn( self.layers[layerName].collidedLayers, name) then
+            table.insert( self.layers[layerName].collidedLayers, name )
         end
-    end
-    for _, name in pairs(collidedLayersNames) do
-        table.insert( self.layers[layerName].collidedLayers, name )
     end
 end
 
 function PhysicsProcessor:addActionLayers( layerName, actionLayersNames )
-	-- Добавить для слоя слои с которыми он взаимодействует
     for _, name in pairs(actionLayersNames) do
-    	local index = findIn(self.layers, name)
-        if index == -1 then
-        	-- Удалить слой из списка
+        if isIn(self.layers, name) and not isIn( self.layers[layerName].actionLayers, name)then
+            table.insert( self.layers[layerName].actionLayers, name )
         end
     end
-    for _, name in pairs(actionLayersNames) do
-        table.insert( self.layers[layerName].actionLayers, name )
-    end
 end
 
-function PhysicsProcessor:registerObject( object, layer, type, linkedTo)
-	-- Создаётся коллайдер связанный с данным объектом
-	table.insert( self.objects, object )
-	-- Указывается Тип лейера для коллайдера
-	self.collider.layer = layer
-	-- Настраивается физичность объекта в соответствии с указанным типом: 
+function PhysicsProcessor:registerObject( object, x, y, layer, type, linkedTo)
+    -- Добавляется объект в список физичных объектов
+    table.insert( self.objects, object )
+    -- Указывается Тип лейера для коллайдера
+    self.collider.layer = layer
+    -- Настраивается физичность объекта в соответствии с указанным типом: 
 
-	-- SolidBody - Твёрдый объект, который не подвержен гравитации и не отталкивается - гравитация 0,0, чек на игнор взаимодействий и отталкиваний
-	if type == 'SolidBody' then
-		PhysicsObject.init( object, { 0, 0 }, 0, false )
-
-	-- RigidBody - тело, подверженное гравитации. Отталкивается. - никаких изменений
-	elseif type == 'RigidBody' then
-		PhysicsObject.init( object, { 0, 1 }, 10 )
-
-	-- TriggerBody - не подвержено гравитации и не отталкивает. Но регистрирует столкновения - Гравитация 0,0, чек на игнор отталкиваний
-	elseif type == 'TriggerBody' then
-		PhysicsObject.init( object, { 0, 0 }, 0, false )
-	end
-	
-	-- По сути инитится PhysicsObject в нужном объекте с указанными параметрами
+    -- SolidBody - Твёрдый объект, который не подвержен гравитации и не отталкивается - гравитация 0,0, чек на игнор взаимодействий и отталкиваний
+    -- RigidBody - тело, подверженное гравитации. Отталкивается. - никаких изменений
+    PhysicsObject.init( object, x, y, self.objectsTypes[type].gravity, self.objectsTypes[type].maxSpeed, self.objectsTypes[type].isColliding )
+    -- По сути инитится PhysicsObject в нужном объекте с указанными параметрами
 end
 
-function PhysicsProcessor:CalculateCollisions()
-	-- Найти объект с линкованными коллайдерами и пройтись по всем разом, рассчитывая с единым дельта вектором для всех
+function PhysicsProcessor:calculateCollisions()
+-- Найти объект с линкованными коллайдерами и пройтись по всем разом, рассчитывая с единым дельта вектором для всех
     for ind, object in pairs(self.objects) do
+        -- Взять все коллизии объекта
         local collisions = self.HC:collisions(object.collider)
         for shape, delta in pairs(collisions) do
-        	local collidedObject
-            for ind, secondObject in pairs(self.objects) do
-                collidedObject = secondObject.collider == shape and secondObject or collideObject
+            -- Найти объект с которым сколлизировал среди зарегистрированных
+            local collidedObject
+            for ind, physicsObject in pairs(self.objects) do
+                collidedObject = physicsObject.collider == shape and physicsObject or collideObject
             end
+
+            -- Для найденного объекта определить слой и определить находится ли он в группах коллизирующих или взаимодействующих слоёв основного объекта
             if collidedObject then  
-    			local collideIndex = findIn(self.layers[object.collider.layer].collidedLayers, collidedObject.collider.layer)
-  				if collideIndex > -1 and object.isColliding then
-  					if object.regiterCollision then
-  						object:regiterCollision(object, collideObject, delta)
-  					else
-  						object:defaultCollisionReaction(object, delta)
-  					end
-  				end
-    			local actionIndex = findIn(self.layers[object.collider.layer].actionLayers, collidedObject.collider.layer)
-  				if actionIndex > -1 then
-  					if object.regiterAction then
-                		object:regiterAction(object, collideObject, delta)
-                	end
-  				end
+                -- Если с данным слоем основной объект коллизирует, используем его функцию коллизий или базовую 
+                if isIn(self.layers[object.collider.layer].collidedLayers, collidedObject.collider.layer) and object.isColliding then
+                    if object.regiterCollision then
+                        object.regiterCollision(object, collideObject, delta)
+                    else
+                        object.defaultCollisionReaction(object, delta)
+                    end
+                end
+                if isIn(self.layers[object.collider.layer].actionLayers, collidedObject.collider.layer) then
+                    if object.registerAction then
+                        object.registerAction(object, collideObject, delta)
+                    end
+                end=
             end
         end
     end
 end
 
 function PhysicsProcessor:update( dt )
-
-	-- Производится вызов трёх отдельны модулей:
-	-- Модуль просчёта взаимодействия объектов
-	-- Модуль движения всех объектов зарегистрированных в игре
-	-- Модуль действий с анимацией
-	self:CalculateCollisions()
-	MovingProcessor:update( dt )
+    -- Производится вызов трёх отдельны модулей:
+    -- Модуль просчёта взаимодействия объектов
+    -- Модуль движения всех объектов зарегистрированных в игре
+    -- Модуль действий с анимацией
+    self:calculateCollisions()
+    self.movingProcessor:update( dt )
 end
 
 return PhysicsProcessor
